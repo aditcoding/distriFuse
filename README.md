58eadff06830159816b3899251cbe0cbe7511f15
adi <findadit@gmail.com>
2015-10-30 20:42:14 -0500

    changes

---
+++ README.md
@@ -0,0 +1,54 @@
+jnr-fuse
+==
+[![Build Status](https://travis-ci.org/SerCeMan/jnr-fuse.svg?branch=master)](https://travis-ci.org/SerCeMan/jnr-fuse)  [ ![Download](https://api.bintray.com/packages/serce/maven/jnr-fuse/images/download.svg) ](https://bintray.com/serce/maven/jnr-fuse/_latestVersion)  [![Dependency Status](https://www.versioneye.com/user/projects/55798fad666636001e000005/badge.svg?style=flat)](https://www.versioneye.com/user/projects/55798fad666636001e000005)
+
+jnr-fuse is FUSE implementation in java using Java Native Runtime (JNR). 
+
+## Project Goals
+Main goal of the project is to provide easy way to create high-performance filesystem in userspace.
+
+## About technologies
+[FUSE](http://fuse.sourceforge.net/) (Filesystem in Userspace)  is an OS mechanism for unix-like OS that lets non-privileged users create their own file systems without editing kernel code. 
+
+[Java Native Runtime](https://github.com/jnr/jnr-ffi) (JNR) is high-performance Java API for binding native libraries and native memory.
+
+## Get it
+### Gradle
+```groovy
+repositories {
+    jcenter()
+}
+
+dependencies {
+    compile 'com.github.serceman:jnr-fuse:0.1'
+}
+````
+### Maven
+```xml
+    <repositories>
+        <repository>
+            <id>central</id>
+            <name>bintray</name>
+            <url>http://jcenter.bintray.com</url>
+        </repository>
+    </repositories>
+
+    <dependencies>
+        <dependency>
+            <groupId>com.github.serceman</groupId>
+            <artifactId>jnr-fuse</artifactId>
+            <version>0.1</version>
+        </dependency>
+    </dependencies>
+```
+
+## How to use
+For implementing your own filesystem you need just extend FuseStubFS class and implement methods you need. 
+
+See [some examples](https://github.com/SerCeMan/jnr-fuse/tree/master/src/main/java/ru/serce/jnrfuse/examples).
+
+## Supported platforms
+| Supported platforms |     |      |
+|---------------------|-----|------|
+| Linux               | x64 | x86  |
+
--- src/main/java/ru/serce/jnrfuse/examples/HelloFuse.java
+++ src/main/java/ru/serce/jnrfuse/examples/HelloFuse.java
@@ -1,6 +1,13 @@
 package ru.serce.jnrfuse.examples;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import jnr.ffi.Pointer;
+import jnr.ffi.types.mode_t;
 import jnr.ffi.types.off_t;
 import jnr.ffi.types.size_t;
 import ru.serce.jnrfuse.ErrorCodes;
@@ -9,80 +16,164 @@ import ru.serce.jnrfuse.FuseStubFS;
 import ru.serce.jnrfuse.struct.FileStat;
 import ru.serce.jnrfuse.struct.FuseFileInfo;
 
-import java.nio.file.Paths;
-import java.util.Objects;
-
 public class HelloFuse extends FuseStubFS {
 
     public static final String HELLO_PATH = "/hello";
     public static final String HELLO_STR = "Hello World!";
+    private static final String MOUNT_PATH = "/home/adi/zfs";
 
     @Override
     public int getattr(String path, FileStat stat) {
-        int res = 0;
+        /*int res = 0;
         if (Objects.equals(path, "/")) {
             stat.st_mode.set(FileStat.S_IFDIR | 0755);
             stat.st_nlink.set(2);
         } else if ("/hello".equals(path)) {
-            stat.st_mode.set(FileStat.S_IFREG | 0444);
+            log("Reading attrs for: " + path);
+            stat.st_mode.set(FileStat.S_IFREG | 0777);
             stat.st_nlink.set(1);
             stat.st_size.set(HELLO_STR.getBytes().length);
         } else {
             res = -ErrorCodes.ENOENT();
         }
-        return res;
+        return res;*/
+
+        /*File f = new File(MOUNT_PATH + path);
+        if (f.isFile()) {
+            stat.st_mode.set(0777);
+            stat.st_size.set(f.length());
+            stat.st_nlink.set(1);
+            stat.st_blocks.set(((f.length() + 511L) / 512L));
+            return 0;
+        }
+        else if (f.isDirectory()) {
+            stat.st_mode.set(FileStat.S_IFDIR | 0755);
+            return 0;
+        }
+        return -ErrorCodes.ENOENT();*/
+
     }
 
     @Override
     public int readdir(String path, Pointer buf, FuseFillDir filter, @off_t long offset, FuseFileInfo fi) {
-        if (!"/".equals(path)) {
+       /* if (!"/".equals(path)) {
             return -ErrorCodes.ENOENT();
-        }
-
+        }*/
+        log("Reading dir: " + path);
         filter.apply(buf, ".", null, 0);
         filter.apply(buf, "..", null, 0);
-        filter.apply(buf, HELLO_PATH.substring(1), null, 0);
+
+        /*File f = new File(MOUNT_PATH + path);
+        if (f.isDirectory()) {
+            File[] fList = f.listFiles();
+            for (File file : fList) {
+                filter.apply(buf, file.toString(), null, 0);
+            }
+
+        }*/
+        //filter.apply(buf, HELLO_PATH.substring(1), null, 0);
         return 0;
     }
 
     @Override
     public int open(String path, FuseFileInfo fi) {
-        if (!HELLO_PATH.equals(path)) {
+        /*if (!HELLO_PATH.equals(path)) {
             return -ErrorCodes.ENOENT();
+        }*/
+        log("Opening: " + path);
+        return 0;
+    }
+
+    /*@Override
+    public int opendir(final String path, final FuseFileInfo info) {
+        log("Opening dir: " + path);
+        return 0;
+    }*/
+
+    @Override
+    public int read(String path, Pointer buf, @size_t long size, @off_t long offset, FuseFileInfo fi) {
+        /*if (!HELLO_PATH.equals(path)) {
+            return -ErrorCodes.ENOENT();
+        }*/
+
+        //byte[] bytes = HELLO_STR.getBytes();
+        log("Reading from: " + path + " from offset: " + offset);
+        try {
+            Path p = Paths.get(MOUNT_PATH + path);
+            log("Path p: " + p.toString());
+            byte[] bytes = Files.readAllBytes(p);
+            int length = bytes.length;
+            log("byte length: " + length);
+            if (offset < length) {
+                if (offset + size > length) {
+                    size = length - offset;
+                }
+                buf.put(0, bytes, 0, bytes.length);
+            } else {
+                size = 0;
+            }
+            return (int) size;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    @Override
+    public int write(String path, Pointer buf, @size_t long size, @off_t long offset, FuseFileInfo fi) {
+        try {
+            log("Writing to: " + path);
+            byte[] b = new byte[(int) size];
+            buf.get(offset, b, 0, (int) size);
+            FileOutputStream output = new FileOutputStream(MOUNT_PATH + path, true);
+            output.write(b);
+            output.close();
+            return (int) size;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int create(String path, @mode_t long mode, FuseFileInfo fi) {
+        File f = new File(MOUNT_PATH + path);
+        try {
+            log("Creating  new file: " + path);
+            f.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return 0;
     }
 
     @Override
-    public int read(String path, Pointer buf, @size_t long size, @off_t long offset, FuseFileInfo fi) {
-        if (!HELLO_PATH.equals(path)) {
-            return -ErrorCodes.ENOENT();
-        }
-
-        byte[] bytes = HELLO_STR.getBytes();
-        int length = bytes.length;
-        if (offset < length) {
-            if (offset + size > length) {
-                size = length - offset;
-            }
-            buf.put(0, bytes, 0, bytes.length);
+    public int mkdir(String path, @mode_t long mode) {
+        File f = new File(MOUNT_PATH + path);
+        log("Trying to create new dir: " + path);
+        if (f.exists()) {
+            f = null;
+            return -ErrorCodes.EEXIST();
         } else {
-            size = 0;
+            f.mkdir();
         }
-        return (int) size;
-    }
-
-    @Override
-    public int write(String path, Pointer buf, @size_t long size, @off_t long offset, FuseFileInfo fi) {
         return 0;
     }
 
     public static void main(String[] args) {
         HelloFuse stub = new HelloFuse();
         try {
-            stub.mount(Paths.get("/home/adi/Projects/distriAfs/mymnt"), true);
+            stub.mount(Paths.get(MOUNT_PATH), true);
+            //stub.mount(Paths.get("/zfs/mnt"), true);
         } finally {
             stub.umount();
+        }
+    }
+
+    public static void log(Object... obj) {
+        for (Object o : obj) {
+            System.out.println(o);
         }
     }
 }
